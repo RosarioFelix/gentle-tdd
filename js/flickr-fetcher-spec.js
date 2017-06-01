309ab5eec865bd27c14eaf54fd069ad63a326fa9
Rosario Felix <elijan_11@hotmail.com>
2017-06-01 12:10:50 -0500

    eliminar node

---
+++ js/flickr-fetcher-spec.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+describe('FlickrFetcher', function() {
+    it('should exist', function() {
+        var FlickrFetcher = require('./flickr-fetcher.js');
+        expect(FlickrFetcher).to.not.be.undefined;
+    });
+});
